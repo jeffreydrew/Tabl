31e51c0ba06d2400d50e943d7e651e0334b8a441
jeffreydrew <98627285+jeffreydrew@users.noreply.github.com>
2022-12-28 19:31:09 -0500

    working test framework

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,6 @@
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+#add src to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 import update_records
--- tests/test_update_records.py
+++ tests/test_update_records.py
@@ -1,14 +1,9 @@
 import os
 import pytest
+from .context import update_records
 from update_records import *
 
-PATH_TEST_DB = "tests/test_databases/test_team_records.db"
+def test_read_teams():
+    teams = read_teams()
+    assert len(teams) == 8
 
-def test_create_teams_table():
-    # create table
-    create_teams_table(PATH_TEST_DB)
-    # check if table exists
-    assert os.path.exists(PATH_TEST_DB)
-
-    assert len(get_records(PATH_TEST_DB)) == 8
-     
