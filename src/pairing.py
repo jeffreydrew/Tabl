ca5eef13495301a3fea33da27c090ee47e32e8ad
jeffreydrew <98627285+jeffreydrew@users.noreply.github.com>
2022-12-31 18:23:06 -0500

    now round 2 pairings actually work

--- src/pairing.py
+++ src/pairing.py
@@ -1,5 +1,69 @@
 import random
-from update_records import rank_teams
+import sqlite3
+
+PATH_DB = "databases/team_records.db"
+
+# ------------------------------------------------------------------------------------------------
+#                                       Stuff For Pairings
+# ------------------------------------------------------------------------------------------------
+def rank_teams(round_number=2):
+    """
+    for round 2: rankings are based on wins and pd
+    """
+    rankings = {}
+
+    conn = sqlite3.connect(PATH_DB)
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM team_records""")
+    rows = cursor.fetchall()
+
+    condensed_teams = []
+    for row in rows:
+        condensed_teams.append([row[0], row[2], row[6], row[11]])
+
+    # sort by wins first
+    condensed_teams.sort(key=lambda x: x[1], reverse=True)
+
+    # if two teams have same number of wins, sort them by pd
+    for i in range(len(condensed_teams) - 1):
+        if condensed_teams[i][1] == condensed_teams[i + 1][1]:
+            if condensed_teams[i][2] < condensed_teams[i + 1][2]:
+                condensed_teams[i], condensed_teams[i + 1] = (
+                    condensed_teams[i + 1],
+                    condensed_teams[i],
+                )
+
+    # if two teams have same number of wins and pd, sort them by higher team number
+    for i in range(len(condensed_teams) - 1):
+        if (
+            condensed_teams[i][1] == condensed_teams[i + 1][1]
+            and condensed_teams[i][2] == condensed_teams[i + 1][2]
+        ):
+            if int(condensed_teams[i][0]) < int(condensed_teams[i + 1][0]):
+                condensed_teams[i], condensed_teams[i + 1] = (
+                    condensed_teams[i + 1],
+                    condensed_teams[i],
+                )
+
+    p_teams = []
+    d_teams = []
+    for team in condensed_teams:
+        if team[3] == "P":
+            p_teams.append(team)
+        else:
+            d_teams.append(team)
+
+    # put p teams into rankings with key 'P' + str(rank)
+    for i in range(len(p_teams)):
+        rankings["P" + str(i + 1)] = p_teams[i][0]
+        rankings["D" + str(i + 1)] = d_teams[i][0]
+
+    return rankings
+
+
+# ------------------------------------------------------------------------------------------------
+#                                           Pairings
+# ------------------------------------------------------------------------------------------------
 
 
 def round_1_Pairings(teams: list[list[str]]):
@@ -58,10 +122,14 @@ def round_2_Pairings():
     rankings = rank_teams(2)
 
     # for i in half the length of the rankings list:
-    for i in range(len(rankings) // 2):
+    for i in range(1, len(rankings) // 2 + 1):
         pairings.append([rankings["P" + str(i)], rankings["D" + str(i)]])
 
     return pairings
+
+
+# print round 2 pairings
+print(round_2_Pairings())
 
 
 def round_3_Pairings(teams: list[list[str]]):
--- src/update_records.py
+++ src/update_records.py
@@ -455,62 +455,6 @@ def generate_round_ballots(round_number):
         generate_ballot(pairing, path)
 
 
-# ------------------------------------------------------------------------------------------------
-#                                       Stuff For Pairings
-# ------------------------------------------------------------------------------------------------
-def rank_teams(round_number=2):
-    """
-    for round 2: rankings are based on wins and pd
-    """
-    rankings = {}
-    rows = get_records()
-
-    condensed_teams = []
-    for row in rows:
-        condensed_teams.append([row[0], row[2], row[6], row[11]])
-
-    # sort by wins first
-    condensed_teams.sort(key=lambda x: x[1], reverse=True)
-
-    # if two teams have same number of wins, sort them by pd
-    for i in range(len(condensed_teams) - 1):
-        if condensed_teams[i][1] == condensed_teams[i + 1][1]:
-            if condensed_teams[i][2] < condensed_teams[i + 1][2]:
-                condensed_teams[i], condensed_teams[i + 1] = (
-                    condensed_teams[i + 1],
-                    condensed_teams[i],
-                )
-
-    # if two teams have same number of wins and pd, sort them by higher team number
-    for i in range(len(condensed_teams) - 1):
-        if (
-            condensed_teams[i][1] == condensed_teams[i + 1][1]
-            and condensed_teams[i][2] == condensed_teams[i + 1][2]
-        ):
-            if int(condensed_teams[i][0]) < int(condensed_teams[i + 1][0]):
-                condensed_teams[i], condensed_teams[i + 1] = (
-                    condensed_teams[i + 1],
-                    condensed_teams[i],
-                )
-
-    p_teams = []
-    d_teams = []
-    for team in condensed_teams:
-        if team[3] == "P":
-            p_teams.append(team)
-        else:
-            d_teams.append(team)
-
-    # put p teams into rankings with key 'P' + str(rank)
-    for i in range(len(p_teams)):
-        rankings["P" + str(i + 1)] = p_teams[i][0]
-        rankings["D" + str(i + 1)] = d_teams[i][0]
-
-    return rankings
-
-
-print(rank_teams())
-
 # ------------------------------------------------------------------------------
 #                               Misc/Testing Functions
 # ------------------------------------------------------------------------------
