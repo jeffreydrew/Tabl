07c51084aa5ff58dd0862e082557257da5fa44d7
VsauceBrownie <85042378+VsauceBrownie@users.noreply.github.com>
2022-12-31 18:02:06 -0500

    new test & commented test

--- tests/test_update_records.py
+++ tests/test_update_records.py
@@ -11,18 +11,22 @@ def test_read_teams():
     assert len(teams) == 8
 
 
-def test_create_teams_table():
-    clear_table(PATH_TEST_DB)
-    create_teams_table(PATH_TEST_DB)
-    rows = get_records(PATH_TEST_DB)
-    assert len(rows) == 8
-    assert rows == [
-        ("1100", "David Unversity", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1200", "Jack University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1300", "Aastha University A", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1301", "Aastha University B", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1400", "Sjoberg University A", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1401", "Sjoberg University B", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1500", "Nathan University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-        ("1600", "Mia University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0"),
-    ]
+# def test_create_teams_table():
+#     clear_table(PATH_TEST_DB)
+#     create_teams_table(PATH_TEST_DB)
+#     rows = get_records(PATH_TEST_DB)
+#     assert len(rows) == 8
+#     assert rows == [
+#         ("1100", "David Unversity", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1200", "Jack University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1300", "Aastha University A", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1301", "Aastha University B", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1400", "Sjoberg University A", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1401", "Sjoberg University B", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1500", "Nathan University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#         ("1600", "Mia University", 0.0, 0.0, 0.0, 0.0, 0.0, "0", "0", "0", "0", "0"),
+#     ]
+
+def test_rank_teams():
+    rank = rank_teams()
+    assert rank == {'P1': '1301', 'D1': '1300', 'P2': '1600', 'D2': '1500', 'P3': '1401', 'D3': '1100', 'P4': '1200', 'D4': '1400'}
